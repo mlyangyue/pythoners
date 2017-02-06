5cb32e9d1e543f8e9887ba3a12a6fc42b6317033
wangranming <wangranming1989@gmail.com>
2017-02-06 09:55:20 +0800

    新增检查文件夹路径是否存在的工具

--- utils/files_modle.py
+++ utils/files_modle.py
@@ -17,6 +17,23 @@ def tar_files(sdir,suffix):
     os.system("tar zcvf log_{time_suf}.tar.gz *{suffix}".format(time_suf=time_suf,suffix=suffix)) # 执行打包命令
     #os.system("rm -rf *{suffix}".format(suffix=suffix)) # 执行删除命令
 
+'''检查文件夹是否存在'''
+def check_dir(path):
+    """
+    :func   检查文件夹是否存在
+    :param  检测路径
+    """
+    import os
+    res = os.path.exists(path)
+    if res:
+        print "存在路径{path}".format(path=path)
+        return True
+    else:
+        print "吧存在路径{path}".format(path=path)
+    return False
+
+
 if __name__=="__main__":
-    tar_files("/Users/wangranming/myproject/logs",".log")
+    # tar_files("/Users/wangranming/myproject/logs",".log")
+    check_dir("/Users/wangranming/myproject")
 
